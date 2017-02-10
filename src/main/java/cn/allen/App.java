5b890ae1c59a6982b0babc2d4d4a9aca5f9fa880
allenleung86 <londalonda@qq.com>
2017-02-10 20:59:28 +0800

    提交第一版代码,为spring在IDEA上用maven搭建的最基本的框架代码,pom包含org.springframework和junit包

---
+++ src/main/java/cn/allen/App.java
@@ -0,0 +1,13 @@
+package cn.allen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
